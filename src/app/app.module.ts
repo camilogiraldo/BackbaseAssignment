c7961301ffb0d96e6f0f35dc5836e33a016f85dc
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 12:45:55 -0500

    Create materialUI module for reuse through the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialUiModule } from './material-ui/material-ui.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, MaterialUiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
