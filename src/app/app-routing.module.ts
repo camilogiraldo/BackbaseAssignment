84853bffdf4b6e5c58ab4bb39a463167a5d04f35
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 13:15:25 -0500

    Create basic routing for the app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailComponent } from './dashboard/detail/detail.component';
+
+const router: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: '**', redirectTo: 'dashboard' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(router)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialUiModule } from './material-ui/material-ui.module';
+import { DetailComponent } from './dashboard/detail/detail.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, MaterialUiModule],
+  declarations: [AppComponent, DashboardComponent, DetailComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialUiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
