c67d354443fed20fd5ded3fbc76025a6acd1ba05
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 17:19:48 -0500

    create dasboard weather loading and basic template

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { WeatherApiService } from './../weather-api.service';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  currentTemperature;
+  response;
+  tempUnitUsed;
+  cities = [
+    { city: 'Amsterdam', id: '6544881' },
+    { city: 'Vienna', id: '2761369' },
+    { city: 'Lisbon', id: '2267057' },
+    { city: 'London', id: '2643741' },
+    { city: 'Prague', id: '3067696' }
+  ];
 
-  constructor() { }
+  constructor(private weatherService: WeatherApiService) {}
 
   ngOnInit() {
+    const cityIds = this.cities.map(city => city.id);
+    this.weatherService.setInitialConfig();
+    this.getCurrentForecast(cityIds);
   }
 
+  getCurrentForecast(cityIds) {
+    this.weatherService.getCurrentForecast(cityIds).subscribe(data => {
+      this.response = data.list;
+    });
+  }
+
+  unitSelected(tempUnitEvent) {
+    this.weatherService.setUnitsToUse(tempUnitEvent);
+    this.getCurrentForecast(this.cities.map(city => city.id));
+  }
 }
