d4a79626f5441a8844ea7bfb702dc337daa85183
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 17:17:49 -0500

    import needed modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialUiModule } from './material-ui/material-ui.module';
 import { DetailComponent } from './dashboard/detail/detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherApiService } from './weather-api.service';
+import { LoadingSpinnerComponent } from './ui/loading-spinner/loading-spinner.component';
+import { TempTogglerComponent } from './ui/temp-toggler/temp-toggler.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, DetailComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialUiModule],
-  providers: [],
+  declarations: [AppComponent, DashboardComponent, DetailComponent, LoadingSpinnerComponent, TempTogglerComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialUiModule
+  ],
+  providers: [WeatherApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
