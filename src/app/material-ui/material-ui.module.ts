687e16dd17cdff57b3e3697ba824b9fdc1edd8ae
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 20:41:59 -0500

    import materialUI needed modules

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule, MatButtonToggleModule, MatIconModule],
-  exports: [MatButtonToggleModule, MatIconModule]
+  imports: [
+    CommonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonToggleModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
+  ]
 })
 export class MaterialUiModule {}
