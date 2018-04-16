c3a818fc370424c2cdeaf43f6216bb4ef9f6c450
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-04-15 20:40:06 -0500

    import pipe, browserAnimationModule and navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialUiModule } from './material-ui/material-ui.module';
 import { DetailComponent } from './dashboard/detail/detail.component';
 import { HttpClientModule } from '@angular/common/http';
-import { WeatherApiService } from './weather-api.service';
+import { WeatherApiService } from './shared/services/weather-api.service';
 import { LoadingSpinnerComponent } from './ui/loading-spinner/loading-spinner.component';
 import { TempTogglerComponent } from './ui/temp-toggler/temp-toggler.component';
+import { NavigationComponent } from './ui/navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, DetailComponent, LoadingSpinnerComponent, TempTogglerComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    DetailComponent,
+    LoadingSpinnerComponent,
+    TempTogglerComponent,
+    NavigationComponent,
+    CapitalizePipe
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MaterialUiModule
+    MaterialUiModule,
+    BrowserAnimationsModule
   ],
   providers: [WeatherApiService],
   bootstrap: [AppComponent]
